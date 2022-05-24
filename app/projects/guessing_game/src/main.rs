5e7dd73680aa5fea57ba297d5af3d41fd2e2ef1a
1000-x-t30 <nagatomi.takayuki81@gmail.com>
2022-05-24 21:57:21 +0900

    数値当てimpl追加

--- app/projects/guessing_game/src/main.rs
+++ app/projects/guessing_game/src/main.rs
@@ -2,6 +2,27 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: u32
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            // 予想の値は1から100の範囲でなければなりませんが、{}でした
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -13,10 +34,15 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read file.");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
 
         println!("You guessed: {}", guess);
 
